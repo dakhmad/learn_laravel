fd707f912cbaeecbc96458a85140794f1480d358
danilearner <dakhmad140104@gmail.com>
2025-03-30 16:32:12 +0700

    day18 learn CRUD Route Form Laravel 11

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,12 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// Create job
 Route::get('/jobs/create', function() {
     return view('jobs.create');
 });
 
+// Show job
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
     
@@ -28,6 +30,7 @@ Route::get('/jobs/{id}', function ($id) {
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store job
 Route::post('/jobs', function() {
     // validation
     request()->validate([
@@ -41,6 +44,59 @@ Route::post('/jobs', function() {
         'employer_id' => 1,
     ]);
 
+    return redirect('/jobs');
+});
+
+// Edit job
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    
+    // dd($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update job
+Route::patch('/jobs/{id}', function ($id) {
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    // authorize (On hold...)
+
+    // update the job
+    $job = Job::findOrFail($id);
+    
+    // and persist
+    // // cara sama
+    // $job->title = request('title');
+    // $job->salary = request('salary');
+    // $job->save();
+
+    // cara sama
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    // redirect to the job page
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy job
+Route::delete('/jobs/{id}', function ($id) {
+    // authorize (On hold...)
+    // delete the job
+
+    // cara panjang
+    // $job = Job::findOrFail($id);
+    // $job->delete();
+
+    // Alternative tercepat
+    $job = Job::findOrFail($id)->delete();
+
+    // redirect
     return redirect('/jobs');
 });
 
