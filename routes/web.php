8beb27cad75a526257e16b2a897fe0da775041c2
danilearner <dakhmad140104@gmail.com>
2025-03-30 17:21:45 +0700

    day19 tips 1,2,3,4, dan 5. ada notes. group route menggunakan controller Route::controller(JobController::class)->group(function() { kumpulan ruter });

--- routes/web.php
+++ routes/web.php
@@ -4,51 +4,45 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 
-// Belajar laravel di Laracast YouTube Channel
-// Route::get('/', function () {
-//     return view('laracast.home');
-// }); // panjang
+// Catatan
+/*
+    Tips dalam route
+    1. Akses model dan deklarasi variabel untuk simpan nilai model dapat dipindahkan tugas menjadi pengisi parameter function
+    2. Menggunakan Controller
+    3. Persingkat dengan Route::view('/alamat-rute', 'view.blade');
+    4. Cek Route list di terminal. php artisan route:list --except-vendor
+    5. Buat group controller dengan Route::controller(ExpController::class)->group(function(){ kumpulan rute disini });
+*/
 
 // Cara singkat
 Route::view('/','laracast.home');
-
-// Operasi Job
-Route::get('/jobs', [JobController::class, 'index']);
-Route::get('/jobs/create', [JobController::class, 'create']);
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-Route::post('/jobs', [JobController::class, 'store']);
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
-// Route::get('/contact', function () {
-//     return view('laracast.contact');
-// }); // panjang caranya
-
 Route::view('/contact', 'laracast.contact');
 
+// Operasi Job
+Route::controller(JobController::class)->group(function () {
+    Route::get('/jobs',  'index');
+    Route::get('/jobs/create',  'create');
+    Route::get('/jobs/{job}',  'show');
+    Route::post('/jobs',  'store');
+    Route::get('/jobs/{job}/edit',  'edit');
+    Route::patch('/jobs/{job}',  'update');
+    Route::delete('/jobs/{job}',  'destroy');
+});
+
 // Ini hanya uji coba
-Route::get('/users', function(){
-    return view('laracast.users', [
-        'users' => User::all(),
-    ]);
-});
+// Route::get('/users', function(){
+//     return view('laracast.users', [
+//         'users' => User::all(),
+//     ]);
+// });
 
-Route::get('/users/{id}', function($id){
-    $user = User::find($id);
+// Route::get('/users/{id}', function($id){
+//     $user = User::find($id);
 
-    return view('laracast.user', ['user' => $user]);
-});
+//     return view('laracast.user', ['user' => $user]);
+// });
 
 // Homework d5
-Route::get('/hw-d5', function () {
-    return view('homework.d5.home');
-});
-
-Route::get('/hw-d5-about', function () {
-    return view('homework.d5.about');
-});
-
-Route::get('/hw-d5-contact', function () {
-    return view('homework.d5.contact');
-});
+// Route::view('/hw-d5', 'homework.d5.home');
+// Route::view('/hw-d5-about', 'homework.d5.about');
+// Route::view('/hw-d5-contact', 'homework.d5.contact');
